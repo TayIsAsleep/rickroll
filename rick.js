788edb2130f1eb18d5adbc2bdf2d76596a4208f1
TayIsAsleep <drpeperoni@outlook.com>
2021-06-14 01:38:43 +0200

    yes

---
+++ rick.js
@@ -0,0 +1,26 @@
+try{clearInterval(t);}catch(error){}
+var t=setInterval(function(){
+	let l = "https://tinyurl.com/2thp8jaa"
+	document.querySelectorAll("img").forEach(e=>{
+		if (e.src != l){
+			e.src=l
+		}
+	})
+	document.querySelectorAll(".guildIcon-2Tpnad").forEach(e=>{
+		if (e.style.backgroundImage != `url(${l})`){
+			e.style.backgroundImage=`url(${l})`
+		}	
+	})
+	
+	document.querySelectorAll(".embedMedia-1guQoW, .messageAttachment-1aDidq").forEach(e=>{
+		let a = document.createElement("img")
+		a.src = l
+		e.replaceWith(a)
+	})
+
+	document.querySelectorAll(".overflow-WK9Ogt, .messageContent-2qWWxC").forEach(e=>{
+		e.innerHTML = "You got Rick Rolled!"
+	})
+
+
+},100);
